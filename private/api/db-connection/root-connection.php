d251245a057bda4efe20ad53deebb10941a175bb
Jakob <jako2889@stud.kea.dk>
2020-04-20 12:33:52 +0200

    added db connection & added get user api

---
+++ private/api/db-connection/root-connection.php
@@ -0,0 +1,19 @@
+<?php
+try{
+
+    $db_user_name = 'root';
+    $db_password = '';
+    //
+    $db_host = '127.0.0.1';
+    $db_name = 'classrooms';
+    $db_connection = "mysql:host=$db_host; dbname=$db_name; charset=utf8mb4";
+    //
+    $db_options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    ];
+    $db = new PDO($db_connection, $db_user_name, $db_password);
+
+}catch(PDOException $ex){
+    echo $ex;
+}
