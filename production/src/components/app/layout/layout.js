9ad46b0c46b951c39d1aaccfcff3750327c1b31d
Magnus V Jensen <magnusvjensen@gmail.com>
2020-05-17 21:43:23 +0200

    menu started

--- production/src/components/app/layout/layout.js
+++ production/src/components/app/layout/layout.js
@@ -1,15 +1,66 @@
 import React from "react"
 import { GlobalStyle } from "../../../styles/global/globalstyles"
+// GLOBALS
+import { size, breakpoints } from "../../../styles/global/breakpoints"
+import { colors } from "../../../styles/global/colors"
+import { headings, bodyText, caption } from "../../../styles/global/typography"
+// ICONS
+import { RiMenu2Line } from "react-icons/ri"
+import { FaRegUserCircle } from "react-icons/fa"
+
+import styled from "styled-components"
+// STYLED
+
+// LAYOUT
+const Wrapper = styled.div``
+
+const Top_container = styled.div`
+  background-color: ${colors.super_dark_purple};
+  height: 60px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 10px;
+
+  /* ! MAKE EFFECTS GLOBALS*/
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  position: fixed;
+
+  & > div {
+    align-self: center;
+  }
+`
+const Bottom_container = styled.div`
+  padding-top: 60px;
+`
+
+const Page_title = styled.div`
+  color: ${colors.sand};
+  ${bodyText.normalTextReg_16px}
+`
 
 const Layout = props => {
   return (
-    <>
+    <div>
       <GlobalStyle />
-      <head>
-        <nav>Layout NA</nav>
-        {props.children}
-      </head>
-    </>
+      <Wrapper>
+        <Top_container>
+          <div>
+            <RiMenu2Line color={colors.orange} size="30px" />
+          </div>
+          <div>
+            <Page_title>My ClassRooms</Page_title>
+          </div>
+          <div>
+            <FaRegUserCircle color={colors.orange} size="30px" />
+          </div>
+        </Top_container>
+        <Bottom_container>
+          <div>MENU</div>
+          <main>{props.children}</main>
+        </Bottom_container>
+      </Wrapper>
+    </div>
   )
 }
 
---
+++ production/src/pages/app/my-classrooms/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../../../components/app/layout/layout"
+
+const My_classrooms = () => {
+  return (
+    <Layout>
+      <h1>THIS IS MY CLASSROOMS</h1>
+    </Layout>
+  )
+}
+
+export default My_classrooms
