e8d32af0571f10eb712d5252a432bf00d0fbd1d4
Magnus V Jensen <magnusvjensen@gmail.com>
2020-05-18 11:35:33 +0200

    animation in

--- production/src/components/app/layout/layout.js
+++ production/src/components/app/layout/layout.js
@@ -1,5 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import { GlobalStyle } from "../../../styles/global/globalstyles"
+import { Link } from "gatsby"
+import Logo_2 from "../../../assets/logo/logo_2"
+import Logo_3 from "../../../assets/logo/logo_3"
 // GLOBALS
 import { size, breakpoints } from "../../../styles/global/breakpoints"
 import { colors } from "../../../styles/global/colors"
@@ -7,6 +10,9 @@ import { headings, bodyText, caption } from "../../../styles/global/typography"
 // ICONS
 import { RiMenu2Line } from "react-icons/ri"
 import { FaRegUserCircle } from "react-icons/fa"
+import { FiSearch } from "react-icons/fi"
+import { MdClose } from "react-icons/md"
+import { BsQuestionCircle } from "react-icons/bs"
 // IMPORT STYLED COMPONENTS
 import {
   Wrapper,
@@ -14,16 +20,30 @@ import {
   Bottom_container,
   Page_title,
   Nav,
+  Nav_head,
+  Logo_con,
 } from "./layout_styles"
 
 const Layout = props => {
+  // STATE
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenuOpen = e => {
+    e.preventDefault()
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <div>
       <GlobalStyle />
       <Wrapper>
         <Top_container>
           <div>
-            <RiMenu2Line color={colors.orange} size="30px" />
+            <RiMenu2Line
+              onClick={e => toggleMenuOpen(e)}
+              color={colors.orange}
+              size="30px"
+            />
           </div>
           <div>
             <Page_title>My ClassRooms</Page_title>
@@ -33,12 +53,44 @@ const Layout = props => {
           </div>
         </Top_container>
         <Bottom_container>
-          <Nav>
+          <Nav menuOpen={menuOpen}>
+            <Nav_head menuOpen={menuOpen}>
+              <Logo_con>
+                <Logo_2 />
+              </Logo_con>
+              <div className="close">
+                <MdClose
+                  onClick={e => toggleMenuOpen(e)}
+                  color={colors.orange}
+                  size="30px"
+                />
+              </div>
+            </Nav_head>
             <ul>
-              <li>Logo x</li>
-              <li>menu 1</li>
-              <li>menu 2</li>
-              <li>menu 3</li>
+              <li>
+                <Link to="#">
+                  <div className="icon">
+                    <Logo_3 />
+                  </div>
+                  <div className="text">My ClassRooms</div>
+                </Link>
+              </li>
+              <li>
+                <Link to="#">
+                  <div className="icon">
+                    <FiSearch color={colors.orange} size="100%" />
+                  </div>
+                  <div className="text">Browse ClassRooms</div>
+                </Link>
+              </li>
+              <li>
+                <Link to="#">
+                  <div className="icon">
+                    <BsQuestionCircle color={colors.orange} size="100%" />
+                  </div>
+                  <div className="text">Help</div>
+                </Link>
+              </li>
             </ul>
           </Nav>
           <main>{props.children}</main>
--- production/src/components/app/layout/layout_styles.js
+++ production/src/components/app/layout/layout_styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 // GLOBALS
 import { size, breakpoints } from "../../../styles/global/breakpoints"
 import { colors } from "../../../styles/global/colors"
@@ -35,14 +35,21 @@ export const Bottom_container = styled.div`
 `
 
 export const Nav = styled.nav`
-  border: 1px solid green;
   margin-top: -60px;
   position: fixed;
   z-index: 8888;
   width: 100vw;
   height: 100vh;
-  opacity: 1;
   background-color: rgba(0, 0, 0, 0.75);
+  display: none;
+
+  ${props =>
+    props.menuOpen
+      ? css`
+          display: block;
+        `
+      : css``}
+
   ul {
     width: 260px;
     min-width: 260px;
@@ -50,5 +57,94 @@ export const Nav = styled.nav`
     color: white;
     background-color: ${colors.super_dark_purple};
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+    /* ANIMATION */
+    ${props =>
+      props.menuOpen
+        ? css`
+            animation: in 0.3s ease-in-out;
+            animation-timing-function: cubic-bezier(1, 0.145, 0.26, 0.98);
+
+            @keyframes in {
+              from {
+                transform: translateX(-260px);
+              }
+              to {
+                transform: translateX(0px);
+              }
+            }
+          `
+        : css``}
+
+    li {
+      a {
+        height: 50px;
+        align-self: center;
+        color: ${colors.white};
+        border-bottom: 1px solid ${colors.dark_purple};
+        display: flex;
+        padding-left: 15px;
+        text-decoration: none;
+        &:hover {
+          cursor: pointer;
+          color: ${colors.sand};
+        }
+
+        .icon {
+          width: 25px;
+          align-self: center;
+        }
+        .text {
+          align-self: center;
+          margin-left: 15px;
+          ${bodyText.normalTextReg_16px}
+        }
+      }
+    }
   }
+`
+export const Nav_head = styled.div`
+  height: 60px;
+  width: 260px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  position: relative;
+  background-color: ${colors.super_dark_purple};
+  display: flex;
+  padding: 0px 15px;
+  justify-content: space-between;
+
+  /* ANIMATION */
+  ${props =>
+    props.menuOpen
+      ? css`
+          animation: in 0.3s ease-in-out;
+          animation-timing-function: cubic-bezier(1, 0.145, 0.26, 0.98);
+
+          @keyframes in {
+            from {
+              transform: translateX(-260px);
+            }
+            to {
+              transform: translateX(0px);
+            }
+          }
+        `
+      : css``}
+
+  & > div {
+    align-self: center;
+  }
+
+  .close {
+    &:hover {
+      cursor: pointer;
+      svg {
+        fill: ${colors.sand};
+      }
+    }
+  }
+`
+export const Logo_con = styled.div`
+  width: 150px;
+  transform: translateY(6px);
 `
