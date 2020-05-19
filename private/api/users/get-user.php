ac2380f48c80308df2c5c3d4e5675f48aa2bd069
Jakob <jako2889@stud.kea.dk>
2020-05-19 11:37:06 +0200

    git reset

---
+++ private/api/users/get-user.php
@@ -0,0 +1,26 @@
+<?php
+require_once('../db-connection/root-connection.php');
+
+try{
+
+    $email = $_POST['email'];
+
+    //Prepare SQL query
+    $q = $db->prepare('SELECT * FROM users WHERE email = :email');
+
+    //Bind Values
+    $q->bindValue(':email', $email);
+
+    //Execute SQL query
+    $q->execute();
+
+    //FETCH DATA
+    $jData = $q->fetchAll();
+    
+    http_response_code(200);
+    header('Content-Type: application/json');
+    echo json_encode($jData);
+
+}catch(PDOException $ex){
+    echo $ex;
+}
