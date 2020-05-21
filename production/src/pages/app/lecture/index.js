cf2d6c3bfb538106c95193cc481ccb79017d2da2
Magnus V Jensen <magnusvjensen@gmail.com>
2020-05-21 19:51:02 +0200

    added lectue and steps

--- production/src/pages/app/lecture/index.js
+++ production/src/pages/app/lecture/index.js
@@ -8,9 +8,10 @@ const Lecture_page = ({ location }) => {
   // TODO: WHAT HAPPENS IF LOCATION.state.lecture_id is på provided
   // FETCH CLASSROOM
   const url =
-    "/private/api/lectures/get-lecture.php?lecture_id=" +
+    "/private/api/lectures/get-lecture-and-steps.php?lecture_id=" +
     location.state.lecture_id
   const res = useFetch(url, {})
+  const lecture = res.response
 
   const List = styled.ul`
     max-width: 800px;
@@ -27,12 +28,38 @@ const Lecture_page = ({ location }) => {
 
   return (
     <Layout page_title="this is a lecture">
-      {!res.response ? (
+      {!lecture ? (
         <div>LOADING...</div>
       ) : (
         <div>
-          <h2> {res.response.title}</h2>
-          <div> {res.response.description}</div>
+          <h2> {lecture.title}</h2>
+          <div> {lecture.description}</div>
+          <ul>
+            {lecture.steps.map(e => {
+              return (
+                <div>
+                  <h3>{e.title}</h3>
+                  <p>{e.theoryText}</p>
+                  <h4>question?</h4>
+                  <p>{e.question}</p>
+                  <h4>Answers</h4>
+                  {e.answers.map(e => {
+                    return (
+                      <div
+                        style={
+                          e.isCorrect === "1"
+                            ? { color: "Green" }
+                            : { colors: "black" }
+                        }
+                      >
+                        Answer: {e.answerValue}
+                      </div>
+                    )
+                  })}
+                </div>
+              )
+            })}
+          </ul>
         </div>
       )}
     </Layout>
