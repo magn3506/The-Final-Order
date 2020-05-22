90925146be786496f20d2d318b7deaa6fb901df3
Magnus V Jensen <magnusvjensen@gmail.com>
2020-05-22 20:40:52 +0200

    added classroom list item component

---
+++ production/src/components/app/molecules/classroom_list_item/classroom_list_item_one.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Classroom_list_item_one = () => {
+  return (
+    <li>
+      <div>
+        <img src="https://www.placecage.com/c/100/100" alt="classroom image" />
+        <div>
+          <h3>Database Design</h3>
+          <h4>By Magnus Jensen</h4>
+        </div>
+      </div>
+      <div>
+        <p>
+          NASA scientists concur prolonged visual exposure to Chuck Norris can
+          result in blindness, nausea and partial para...
+        </p>
+      </div>
+    </li>
+  )
+}
+
+export default Classroom_list_item_one
---
+++ production/src/components/app/molecules/classroom_list_item/classroom_list_item_one.stories.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Item from "./classroom_list_item_one"
+
+export default {
+  title: "Button",
+  component: Button,
+}
+
+export const item_1 = () => <Item>Hello Button</Item>
