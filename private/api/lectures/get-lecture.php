c94508344430e51b0e2977ce4c0b1c888f78e8d0
Magnus V Jensen <magnusvjensen@gmail.com>
2020-05-25 11:18:30 +0200

    updated get lecture, steps API. Now all info evailible in get lectue

--- private/api/lectures/get-lecture.php
+++ private/api/lectures/get-lecture.php
@@ -1,4 +1,7 @@
 <?php
+// acces local_server variables
+include('../../local_server_path.php'); 
+
 require_once('../db-connection/root-connection.php');
 try{
     if(isset($_GET['lecture_id'])){
@@ -22,7 +25,11 @@ try{
             $j_lecture_obj->title = $jData[0]->title;
             $j_lecture_obj->description = $jData[0]->description;
             $j_lecture_obj->classroomID = $jData[0]->classroomID;
-
+            // GET ALL STEPS
+            $get_steps_api = $local_server_path . '/private/api/steps/get-steps.php?lecture_id='.$lecture_id;
+            $s_steps_data = file_get_contents($get_steps_api);
+            $j_lecture_obj->steps = json_decode($s_steps_data);
+            
             // RESPONSE CODE & HEADER
             http_response_code(200);
             header('Content-Type: application/json');
